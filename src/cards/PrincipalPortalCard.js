d8abd03c650f0645b2a5c0c5067fe241ff1ecc58
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-09-12 13:14:31 +0530

    create individual item cards for principal page

--- src/cards/PrincipalPortalCard.js
+++ src/cards/PrincipalPortalCard.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { FaSchoolFlag } from 'react-icons/fa6';
+import React from "react";
+import { FaSchoolFlag } from "react-icons/fa6";
 
-const PrincipalPortalCard = () => {
+const PrincipalPortalCard = (props) => {
   return (
-    <div className="  flex flex-col justify-center items-center w-56 h-56 bg-purple-500 rounded-3xl ">
-          <div className= "bg-white h-24 w-24 rounded-full flex justify-center items-center ">
-            <FaSchoolFlag className= 'w-16 h-16'/>
-          </div>
-          
-          <p className="font-xl font-medium text-white">School Tools</p>
-        </div>
-  )
-}
+    <div className="  flex flex-col justify-center items-center w-56 h-56 bg-purple-500 rounded-3xl gap-y-4 ">
+      <div className="bg-white h-24 w-24 rounded-full flex justify-center items-center ">
+        <FaSchoolFlag className="w-16 h-16" />
+      </div>
 
-export default PrincipalPortalCard
+      <p className="font-xl font-medium text-white">School Tools</p>
+    </div>
+  );
+};
+
+export default PrincipalPortalCard;
