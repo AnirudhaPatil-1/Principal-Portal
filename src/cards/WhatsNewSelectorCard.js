8edcf2c25dc9504feb4d1bfa328c9aaa5b74d803
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-09-12 13:20:22 +0530

    item card - whats new selector for section

--- src/cards/WhatsNewSelectorCard.js
+++ src/cards/WhatsNewSelectorCard.js
@@ -1,22 +1,20 @@
-    import React from 'react';
+import React from "react";
 
-    import {FaLocationDot} from "react-icons/fa6";
-    import {AiOutlineDown} from "react-icons/ai";
-    
-    const WhatsNewSelectorCard = () => {
-      return (
-        <div className=' relative flex justify-around items-center w-96 h-24 bg-white rounded-xl shadow border border-zinc-100 gap-'>
-            <div className='w-14 h-14 bg-emerald-500 bg-opacity-20 rounded-full border border-emerald-500 border-opacity-40 flex justify-center items-center'>
-                <FaLocationDot className='w-8 h-8  text-emerald-500'/>
-            </div>
-            <div className=' heading font-medium'>
-                Local District
-            </div>
-            <div>
-                <AiOutlineDown/>
-            </div>
-        </div>
-      )
-    }
-    
-    export default WhatsNewSelectorCard
+import { FaLocationDot } from "react-icons/fa6";
+import { AiOutlineDown } from "react-icons/ai";
+
+const WhatsNewSelectorCard = () => {
+  return (
+    <div className=" relative flex justify-around items-center w-96 h-24 bg-white rounded-xl shadow border border-zinc-100 gap-">
+      <div className="w-14 h-14 bg-emerald-500 bg-opacity-20 rounded-full border border-emerald-500 border-opacity-40 flex justify-center items-center">
+        <FaLocationDot className="w-8 h-8  text-emerald-500" />
+      </div>
+      <div className=" heading font-medium">Local District</div>
+      <div>
+        <AiOutlineDown />
+      </div>
+    </div>
+  );
+};
+
+export default WhatsNewSelectorCard;
