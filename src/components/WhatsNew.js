727fcf779d6844928f1157646306d85a858be29b
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-09-12 13:25:53 +0530

    section added - whats new section added

--- src/components/WhatsNew.js
+++ src/components/WhatsNew.js
@@ -1,16 +1,21 @@
 import React from "react";
 
+import ViewMore from "../assets/ViewMore.svg";
+
 import WhatsNewCard from "../cards/WhatsNewCard";
 import WhatsNewSelectorCard from "../cards/WhatsNewSelectorCard";
+import { FaLocationDot } from "react-icons/fa6";
+import { AiOutlineDown } from "react-icons/ai";
+import { FaSchoolFlag } from "react-icons/fa6";
+import { BiSolidCategoryAlt } from "react-icons/bi";
 
 import { FaLessThan } from "react-icons/fa";
 import { FaGreaterThan } from "react-icons/fa";
 import { AiOutlineClockCircle } from "react-icons/ai";
-import { GrLinkNext } from "react-icons/gr";
 
 const WhatsNew = () => {
   return (
-    <div className=" relative bg-gray-100 p-20">
+    <div className=" relative bg-gray-100 p-20 ">
       <div className="flex justify-between items-center">
         <h1 className="  text-4xl font-semibold">What's New</h1>
 
@@ -22,32 +27,40 @@ const WhatsNew = () => {
           <FaGreaterThan />
         </div>
       </div>
-      {/* <div className=''>
-        <div className='w-96 h-28 bg-white  p-8 flex justify-between items-center flex-wrap rounded-2xl'>
-          <div className='w-32 h-14 bg-white flex justify-center items-center'>
-              <p className='text-sm font-medium capitalize '>Fall ShakeOut EarthQuake Drill</p>
-          </div>
-          <div className='w-32 h-14 bg-emerald-500 rounded-full  flex justify-center items-center'>
-              <div className='heading text-white text-base  flex justify-center items-center gap-1 '> 
-                <AiOutlineClockCircle/> 
-                <p>10-10-2021</p>
+
+      <div className="flex justify-between items-start mt-20 relative">
+        <div className=" flex flex-col justify-items-end relative">
+          <div className=" flex flex-col justify-center items-center gap-5 mb-10">
+            <WhatsNewSelectorCard />
+            {/* <WhatsNewSelectorCard />
+            <WhatsNewSelectorCard /> */}
+            <div className=" relative flex justify-around items-center w-96 h-24 bg-white rounded-xl shadow border border-zinc-100 gap-">
+              <div className="w-14 h-14 bg-emerald-500 bg-opacity-20 rounded-full border border-emerald-500 border-opacity-40 flex justify-center items-center">
+                <FaSchoolFlag className="w-8 h-8  text-emerald-500" />
               </div>
-          </div>
-        </div>
-      </div> */}
-      <div className="flex justify-between items-start mt-20">
-        <div className=" flex flex-col justify-items-end">
-          <div className=" flex flex-col justify-center items-center gap-5">
-            <WhatsNewSelectorCard />
-            <WhatsNewSelectorCard />
-            <WhatsNewSelectorCard />
-          </div>
-          <div className=" w-48 h-20 bg-emerald-500 rounded-xl shadow flex justify-center items-center text-emerald-white gap-2  ">
-            <div className="Heading text-center text-white text-base font-medium capitalize">
-              View More
+              <div className=" heading font-medium">Local District</div>
+              <div>
+                <AiOutlineDown />
+              </div>
             </div>
-            <div>
-              <GrLinkNext className="text-white " />
+            <div className=" relative flex justify-around items-center w-96 h-24 bg-white rounded-xl shadow border border-zinc-100 gap-">
+              <div className="w-14 h-14 bg-emerald-500 bg-opacity-20 rounded-full border border-emerald-500 border-opacity-40 flex justify-center items-center">
+                <BiSolidCategoryAlt className="w-8 h-8  text-emerald-500" />
+              </div>
+              <div className=" heading font-medium">Local District</div>
+              <div>
+                <AiOutlineDown />
+              </div>
+            </div>
+          </div>
+          <div className="text-center mt-5">
+            <div className="w-48 h-20 bg-emerald-500 rounded-xl shadow flex justify-center items-center text-emerald-white gap-2 mx-auto">
+              <div className="Heading text-center text-white text-base font-medium capitalize">
+                View More
+              </div>
+              <div>
+                <img src={ViewMore} alt="view more" />
+              </div>
             </div>
           </div>
         </div>
