f250d793ffff6b146d2db9da4730c0c2bb61a1da
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-09-12 13:22:37 +0530

    section added - actions section added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
         <ul className='flex justify-end items-center gap-7 text-sm font-medium mt-7'>
           <li className='text-emerald-500 border-b-2 border-emerald-500 text-sm font-medium'>Home</li>
           <li className='text-sm font-medium'>Job Aids</li>
-          <li className=' w-28 h-12 bg-emerald-500 rounded-full text-white text-sm font-medium p-2  mr-20 '>Login</li>
+          <li className=' w-28 h-12 bg-emerald-500 rounded-full text-white text-sm font-medium p-2  mr-20 block flex justify-center items-center'>Login</li>
         </ul>
       
     </div>
