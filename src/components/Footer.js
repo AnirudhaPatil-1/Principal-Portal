bd1affa8c176e07f5a0ad04974a71463b6b15a6b
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-09-12 13:26:57 +0530

    section added - footer section added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-gray-900 pt-20 px-20 pb-5 flex flex-col justify-end items-center">
+      <div className=" text-white text-lg font-medium capitalize">
+        Browsers and Operating Systems Compatibility
+      </div>
+      <div className=" text-center text-white text-opacity-60 text-base font-normal capitalize leading-relaxed">
+        © 2021 - LOS ANGELES UNIFIED SCHOOL DISTRICT
+        <br />
+        333 S Beaudry Ave., Los Angeles, CA 90017
+      </div>
+      <div className=" border  b-5 opacity-50"></div>
+      <div className=" text-center">
+        <span className="text-white text-opacity-60 text-sm font-normal ">
+          Design By{" "}
+        </span>
+        <span className="text-white text-sm font-normal underline capitalize ">
+          Dixellart
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
